34446bc96709d0e7aa775e02eaa2624c64f30a02
Chris Kinoti <devkinoti@gmail.com>
2015-08-27 11:48:50 +0300

    managing users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,50 @@
 class Admin::UsersController < Admin::BaseController
+	before_action :find_user, :only => [:show,:edit,:update,:destroy]
   def index
+  	@users = User.all
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:notice] = "User created successfully"
+  		redirect_to admin_users_path
+  	else
+  		flash[:alert] = "User has not been created"
+  		render :action => "new"
+  	end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  	if params[:user][:password].blank?
+  		params[:user].delete(:password)
+  	end
+
+  	if @user.update_attributes(user_params)
+  		flash[:notice] = "User has been updated successfully"
+  		redirect_to admin_users_path 
+  	else
+  		flash[:alert] = "User has not been updated"
+  		render :action => "edit"
+  	end
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:first_name,:last_name,:email,:password,:admin)
+  end
+
+  def find_user
+  	@user = User.find(params[:id])
   end
 end
