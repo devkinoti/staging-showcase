531b198501df605f3bdb1e90e89e1d908e537c17
Chris Kinoti <devkinoti@gmail.com>
2015-09-08 18:54:12 +0300

    added validation for cash paid

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ActiveRecord::Base
 	has_many :products, through: :line_items
 
 	validates_with EnoughProductsValidator
+	validates :cash_paid, numericality: { only_integer: true, message: "Please enter a number only" }
 	
 	after_create :calculate_stock
 
