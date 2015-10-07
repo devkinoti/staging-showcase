0d6c313f7d2c1bd04584979252323f6f86bf3779
Chris Kinoti <devkinoti@gmail.com>
2015-10-07 17:34:50 +0300

    removed flash html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  use_growlyflash
+  #use_growlyflash
 
   def after_sign_in_path_for(resource)
   	store_path
